acf4fb3ff86c1e449fa3ae6225ccd9d6804a4c20
amirQaderi <amirqaderi381@gmail.com>
2023-02-19 19:27:18 +0330

    edit project

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="contact-management-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
